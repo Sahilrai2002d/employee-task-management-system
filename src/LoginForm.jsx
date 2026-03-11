b734c468a9249a2d7b8d99e32b47e8fe8a5515b5
Sahil Kumar Rai <sahilrai2002d@gmail.com>
2026-03-12 02:06:20 +0530

    Update LoginForm.jsx

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
+import { useAuth } from '../../AuthContext.jsx';
 import { LogIn } from 'lucide-react';
 
 export const LoginForm = ({ onToggle }) => {
